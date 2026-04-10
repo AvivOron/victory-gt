2b6f0d28bebad63a0cdff7f7b40cd780ab69f3f6
Aviv Oron <avivoron@gmail.com>
2026-04-10 20:59:22 +0300

    basePath

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/prices",
-        destination: "/victory-gt/prices",
-        permanent: false,
-      },
-      {
-        source: "/api/:path*",
-        destination: "/victory-gt/api/:path*",
-        permanent: false,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/victory-gt",
-        destination: "/",
-      },
-      {
-        source: "/victory-gt/:path*",
-        destination: "/:path*",
-      },
-    ];
-  },
+  basePath: "/victory-gt",
 };
 
 export default nextConfig;
