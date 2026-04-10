24285ce5a6b9f185e2cd333004342ba6e9bd619e
Aviv Oron <avivoron@gmail.com>
2026-04-10 21:26:58 +0300

    redirect root to basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/victory-gt",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/victory-gt",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
