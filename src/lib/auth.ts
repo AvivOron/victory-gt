97c56585b4be834d42087f028d78a472e3c542e7
Aviv Oron <avivoron@gmail.com>
2026-04-11 18:29:25 +0300

    fix cookie name

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,13 @@ import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  cookies: {
+    sessionToken: { name: "__Secure-victory.session-token", options: { httpOnly: true, sameSite: "lax", path: "/", secure: true } },
+    callbackUrl: { name: "__Secure-victory.callback-url", options: { httpOnly: true, sameSite: "lax", path: "/", secure: true } },
+    csrfToken: { name: "__Host-victory.csrf-token", options: { httpOnly: true, sameSite: "lax", path: "/", secure: true } },
+    pkceCodeVerifier: { name: "__Secure-victory.pkce.code_verifier", options: { httpOnly: true, sameSite: "lax", path: "/", secure: true } },
+    state: { name: "__Secure-victory.state", options: { httpOnly: true, sameSite: "lax", path: "/", secure: true } },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
